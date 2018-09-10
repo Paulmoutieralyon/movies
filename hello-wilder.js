8ccf9dc141969d840a10ab2e75bf1ea961164ff8
Paul Moutier <42945383+Paulmoutieralyon@users.noreply.github.com>
2018-09-10 20:04:17 +0200

    Create hello-wilder.js

---
+++ hello-wilder.js
@@ -0,0 +1,10 @@
+var movies = prompt(" Tape le nom d'un des trois films : Forest Gump - James Bond - Deadpool !");
+if(movies === "Forest Gump"){
+    alert("Ce film a été sortie en 1994 et réaliser par Robert Zemeckis ! Qui a recu six Oscars !!")
+}
+else if(movies === "James Bond"){
+    alert("Ce film a été créer par un écrain Ian Fleming en 1953 !")
+}
+else if(movies === "Deadpool"){
+    alert("Ce film a été créer par un Tim Miller sorti en 2016, il est super je suis aller le voir !")
+}
